d3e38e7875b5cdcbce8e6a8c59e8dce5a16e74b5
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2023-11-12 01:43:44 +0100

    added commentaries

--- src/qr_image_publisher.cpp
+++ src/qr_image_publisher.cpp
@@ -8,19 +8,22 @@
 class QRImagePublisher : public rclcpp::Node {
 public:
     QRImagePublisher() : Node("qr_image_publisher") {
-    std::string package_share_directory = ament_index_cpp::get_package_share_directory("ros2_qrcodereader");
-    std::string default_image_path = package_share_directory + "/imgs/bonjourros2.png";
+
+    //default path in case of
+    std::string default_image_path = ament_index_cpp::get_package_share_directory("ros2_qrcodereader") + "/imgs/bonjourros2.png";
 
     this->declare_parameter<std::string>("image_path", default_image_path);
     std::string image_path = this->get_parameter("image_path").as_string();
     RCLCPP_INFO(this->get_logger(), "Image Path : %s", image_path.c_str());
-    cv_image = cv::imread(image_path, cv::IMREAD_COLOR);
 
+    //try getting image from path
+    cv_image = cv::imread(image_path, cv::IMREAD_COLOR);
     if (cv_image.empty()) {
         RCLCPP_ERROR(this->get_logger(), "Failed to load image at %s", image_path.c_str());
         throw std::runtime_error("Failed to load image");
     }
 
+    //create publisher with 1s fresh rate
     publisher = this->create_publisher<sensor_msgs::msg::Image>("/camera/imageraw", 10);
     timer = this->create_wall_timer(
         std::chrono::milliseconds(1000),
@@ -41,7 +44,6 @@ private:
 
 int main(int argc, char* argv[]) {
     rclcpp::init(argc, argv);
-
     rclcpp::spin(std::make_shared<QRImagePublisher>());
     rclcpp::shutdown();
     return 0;
