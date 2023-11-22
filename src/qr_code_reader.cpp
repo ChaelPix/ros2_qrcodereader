1f987b9ad766c69d9abece68d07670c984816bb2
antoine <grs.antoine@outlook.fr>
2023-11-23 00:55:05 +0100

    Add action server for ReadQrCode action

--- src/qr_code_reader.cpp
+++ src/qr_code_reader.cpp
@@ -4,6 +4,8 @@
 #include "cv_bridge/cv_bridge.h"
 #include "opencv2/opencv.hpp"
 #include "opencv2/objdetect.hpp"
+#include "austria_msgs/action/read_qr_code.hpp"
+#include "nav2_util/simple_action_server.hpp"
 #include <zbar.h>
 
 class QRCodeReader : public rclcpp::Node
@@ -20,15 +22,24 @@ public:
         //Subscriber
         subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
             "camera/image_raw", 10, std::bind(&QRCodeReader::image_callback, this, std::placeholders::_1));
-            
+
+        action_server_read_qr_code_ = std::make_unique<nav2_util::SimpleActionServer<austria_msgs::action::ReadQrCode>>(
+            this,
+            "read_qr_code",
+        std::bind(&QRCodeReader::read_qr_code, this));
+
     }
 
 private:
-    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+
+    void read_qr_code()
     {
+        
+        auto result = std::make_shared<austria_msgs::action::ReadQrCode::Result>();
+
         try {
             //Ros Img to OpenCv image
-            cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+            cv_bridge::CvImagePtr cv_ptr = cv_bridge::toCvCopy(image, sensor_msgs::image_encodings::BGR8);
             cv::Mat cv_image = cv_ptr->image;
             cv::cvtColor(cv_image, cv_image, cv::COLOR_BGR2GRAY);
 
@@ -45,8 +56,9 @@ private:
                     //Get qr code content and publishb it
                     std_msgs::msg::String qr_content;
                     qr_content.data = symbol->get_data();
-                    this->publisher_->publish(qr_content);
+                    result->qrcode_data = symbol->get_data();
                     isSucess = true;
+                    
                 }
             }
 
@@ -54,18 +66,29 @@ private:
             {
                 std_msgs::msg::String qr_content;
                 qr_content.data = "FAIL : No QR Code Detected on the Image";
-                this->publisher_->publish(qr_content);
+                result->qrcode_data = "empty";
             }
 
         } catch (cv_bridge::Exception& e) 
         {
             RCLCPP_ERROR(this->get_logger(), "Could not convert image: %s", e.what());
         }
+
+        action_server_read_qr_code_->succeeded_current(result);
     }
+
+    void image_callback(const sensor_msgs::msg::Image::SharedPtr msg)
+    {
+        image = *msg;
+    }
+
+    sensor_msgs::msg::Image image;
 
     zbar::ImageScanner scanner_;
     rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
     rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    std::unique_ptr<nav2_util::SimpleActionServer<austria_msgs::action::ReadQrCode>> action_server_read_qr_code_;
+ 
 };
 
 int main(int argc, char **argv)
